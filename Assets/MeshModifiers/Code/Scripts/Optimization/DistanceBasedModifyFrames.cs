656d01674f372b094f61e23cf4faa4131c3c7242
keenanwoodall <keenanwoodall@gmail.com>
2016-12-07 17:51:08 -0600

    optimization+
    Added a new script that interpolates the Modify Frames value between a
    min and max amount based on how far away the object is from a transform
    (camera)

---
+++ Assets/MeshModifiers/Code/Scripts/Optimization/DistanceBasedModifyFrames.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using MeshModifiers;
+
+[RequireComponent (typeof (ModifierObject))]
+public class DistanceBasedModifyFrames : MonoBehaviour
+{
+	#region Public Properties
+
+	public Transform target;
+
+	[Range (1, 10)]
+	public int
+		minFrames = 2,
+		maxFrames = 10;
+
+	public float
+		minFramesDistance = 0.1f,
+		maxFramesDistance = 50f;
+
+
+	[NonSerialized]
+	public ModifierObject modObject;
+
+	#endregion
+
+
+
+	#region Unity Methods
+
+	void Start ()
+	{
+		if (modObject == null)
+			modObject = GetComponent<ModifierObject> ();
+	}
+
+	void Update ()
+	{
+		if (target == null || modObject == null || !modObject.update)
+			return;
+
+		float distance = Vector3.Distance (modObject.transform.position, target.position);
+
+		modObject.modifyFrames = (int)Mathf.Lerp (minFrames, maxFrames, Mathf.InverseLerp (minFramesDistance, maxFramesDistance, distance));
+	}
+
+	#endregion
+}
