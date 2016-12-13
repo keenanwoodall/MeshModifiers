0d873f3ef3f353599679e403261a414c02594566
keenanwoodall <keenanwoodall@gmail.com>
2016-12-12 18:16:42 -0600

    examples+
    Added a script that allows the user to quit the demo

---
+++ Assets/MeshModifiers/Examples/Scripts/GeneralUse/QuickQuit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickQuit : MonoBehaviour
+{
+	void Update ()
+	{
+		if (Input.GetKeyUp (KeyCode.Escape))
+			Application.Quit ();
+	}
+}
