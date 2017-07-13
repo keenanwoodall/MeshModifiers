651ce6797647683a03dcfe75ccb111a6ad7492b6
keenanwoodall <keenanwoodall@gmail.com>
2017-07-13 13:21:13 -0500

    Renamed bad variables
    Changed the PinchModifier's variables 'sv' and 'sv2' to 'extents' and
    'doubleExtents' respectively.

--- Assets/MeshModifiers/Code/Scripts/MeshModifiers/PinchModifier.cs
+++ Assets/MeshModifiers/Code/Scripts/MeshModifiers/PinchModifier.cs
@@ -9,14 +9,14 @@ public class PinchModifier : MeshModifierBase
 		negativeAxis = Vector3.one,
 		positiveAxis = Vector3.one;
 
-	private Vector3 sv, sv2;
+	private Vector3 extents, doubleExtents;
 
 	public override void PreMod ()
 	{
 		base.PreMod ();
 
-		sv = modObject.GetBounds ().extents;
-		sv2 = sv * 2;
+		extents = modObject.GetBounds ().extents;
+		doubleExtents = extents * 2;
 
 		negativeAxis = Vectorx.Clamp (negativeAxis, Vector3.zero, Vector3.one);
 		positiveAxis = Vectorx.Clamp (positiveAxis, Vector3.zero, Vector3.one);
@@ -26,20 +26,20 @@ public class PinchModifier : MeshModifierBase
 	{
 		var v = basePosition;
 
-		v.x = Mathf.Lerp (v.x, v.x * (positiveAxis.y), Mathf.Clamp01 ((v.y + sv.y) / sv2.y) * (1.0f - positiveAxis.y));
-		v.x = Mathf.Lerp (v.x, v.x * (negativeAxis.y), Mathf.Clamp01 ((-v.y + sv.y) / sv2.y) * (1.0f - negativeAxis.y));
-		v.x = Mathf.Lerp (v.x, v.x * (positiveAxis.z), Mathf.Clamp01 ((v.z + sv.z) / sv2.z) * (1.0f - positiveAxis.z));
-		v.x = Mathf.Lerp (v.x, v.x * (negativeAxis.z), Mathf.Clamp01 ((-v.z + sv.z) / sv2.z) * (1.0f - negativeAxis.z));
+		v.x = Mathf.Lerp (v.x, v.x * (positiveAxis.y), Mathf.Clamp01 ((v.y + extents.y) / doubleExtents.y) * (1.0f - positiveAxis.y));
+		v.x = Mathf.Lerp (v.x, v.x * (negativeAxis.y), Mathf.Clamp01 ((-v.y + extents.y) / doubleExtents.y) * (1.0f - negativeAxis.y));
+		v.x = Mathf.Lerp (v.x, v.x * (positiveAxis.z), Mathf.Clamp01 ((v.z + extents.z) / doubleExtents.z) * (1.0f - positiveAxis.z));
+		v.x = Mathf.Lerp (v.x, v.x * (negativeAxis.z), Mathf.Clamp01 ((-v.z + extents.z) / doubleExtents.z) * (1.0f - negativeAxis.z));
 
-		v.y = Mathf.Lerp (v.y, v.y * (positiveAxis.x), Mathf.Clamp01 ((v.x + sv.x) / sv2.x) * (1.0f - positiveAxis.x));
-		v.y = Mathf.Lerp (v.y, v.y * (negativeAxis.x), Mathf.Clamp01 ((-v.x + sv.x) / sv2.x) * (1.0f - negativeAxis.x));
-		v.y = Mathf.Lerp (v.y, v.y * (positiveAxis.z), Mathf.Clamp01 ((v.z + sv.z) / sv2.z) * (1.0f - positiveAxis.z));
-		v.y = Mathf.Lerp (v.y, v.y * (negativeAxis.z), Mathf.Clamp01 ((-v.z + sv.z) / sv2.z) * (1.0f - negativeAxis.z));
+		v.y = Mathf.Lerp (v.y, v.y * (positiveAxis.x), Mathf.Clamp01 ((v.x + extents.x) / doubleExtents.x) * (1.0f - positiveAxis.x));
+		v.y = Mathf.Lerp (v.y, v.y * (negativeAxis.x), Mathf.Clamp01 ((-v.x + extents.x) / doubleExtents.x) * (1.0f - negativeAxis.x));
+		v.y = Mathf.Lerp (v.y, v.y * (positiveAxis.z), Mathf.Clamp01 ((v.z + extents.z) / doubleExtents.z) * (1.0f - positiveAxis.z));
+		v.y = Mathf.Lerp (v.y, v.y * (negativeAxis.z), Mathf.Clamp01 ((-v.z + extents.z) / doubleExtents.z) * (1.0f - negativeAxis.z));
 
-		v.z = Mathf.Lerp (v.z, v.z * (positiveAxis.x), Mathf.Clamp01 ((v.x + sv.x) / sv2.x) * (1.0f - positiveAxis.x));
-		v.z = Mathf.Lerp (v.z, v.z * (negativeAxis.x), Mathf.Clamp01 ((-v.x + sv.x) / sv2.x) * (1.0f - negativeAxis.x));
-		v.z = Mathf.Lerp (v.z, v.z * (positiveAxis.y), Mathf.Clamp01 ((v.y + sv.y) / sv2.y) * (1.0f - positiveAxis.y));
-		v.z = Mathf.Lerp (v.z, v.z * (negativeAxis.y), Mathf.Clamp01 ((-v.y + sv.y) / sv2.y) * (1.0f - negativeAxis.y));
+		v.z = Mathf.Lerp (v.z, v.z * (positiveAxis.x), Mathf.Clamp01 ((v.x + extents.x) / doubleExtents.x) * (1.0f - positiveAxis.x));
+		v.z = Mathf.Lerp (v.z, v.z * (negativeAxis.x), Mathf.Clamp01 ((-v.x + extents.x) / doubleExtents.x) * (1.0f - negativeAxis.x));
+		v.z = Mathf.Lerp (v.z, v.z * (positiveAxis.y), Mathf.Clamp01 ((v.y + extents.y) / doubleExtents.y) * (1.0f - positiveAxis.y));
+		v.z = Mathf.Lerp (v.z, v.z * (negativeAxis.y), Mathf.Clamp01 ((-v.y + extents.y) / doubleExtents.y) * (1.0f - negativeAxis.y));
 
 		return v;
 	}
