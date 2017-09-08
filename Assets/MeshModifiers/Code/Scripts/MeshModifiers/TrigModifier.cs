7bb3fbad838373f2eed3b12d22be71650be6a3d6
keenanwoodall <keenanwoodall@gmail.com>
2017-09-08 10:18:34 -0500

    Trig Modifier cleanup
    Trig modifier only affects on axis so its way faster when only wanting a
    sin wave on one axis. If you want it on all axis just add the modifier 3
    times. You can also say what axis you want the modification to be based
    off of. So it could move along the x axis by the z axis etc.

--- Assets/MeshModifiers/Code/Scripts/MeshModifiers/TrigModifier.cs
+++ Assets/MeshModifiers/Code/Scripts/MeshModifiers/TrigModifier.cs
@@ -5,36 +5,40 @@ using MeshModifiers;
 [AddComponentMenu (MeshModifierConstants.ADD_COMP_BASE_NAME + "Trig")]
 public class TrigModifier : MeshModifierBase
 {
-	public Vector3 
-		offset = Vector3.zero,
-		speed = Vector3.one;
+	public float
+		offset,
+		speed;
+	public Axis along;
+	public Axis by;
+	public Trig
+		trig = new Trig (TrigType.Sin);
+	public bool useWorldPos = false;
 
-	public Trig 
-		xAxis = new Trig (TrigType.Sin),
-		yAxis = new Trig (TrigType.Sin),
-		zAxis = new Trig (TrigType.Sin);
+	private Vector3 axisOffset;
 
-	public bool 
-		useWorldPos = false,
-		radial = false;
 
+	public override void PreMod ()
+	{
+		switch (along)
+		{
+			case Axis.X:
+				axisOffset = Vector3.right * offset;
+				break;
+			case Axis.Y:
+				axisOffset = Vector3.up * offset;
+				break;
+			case Axis.Z:
+				axisOffset = Vector3.forward * offset;
+				break;
+		}
+	}
 	protected override Vector3 _ModifyOffset (VertexData vertexData)
 	{
 		var position = vertexData.position;
-		if (radial)
-		{
-			if (useWorldPos)
-				return position * (0.5f + TrigOffset (transform.position + Vector3.one * Vector3.Distance (transform.position, position + transform.position + offset)).magnitude);
-			else
-				return position * (0.5f + TrigOffset (Vector3.one * Vector3.Distance (transform.position, position + transform.position + offset)).magnitude);
-		}
+		if (useWorldPos)
+			return position + TrigOffset (position + transform.position + axisOffset);
 		else
-		{
-			if (useWorldPos)
-				return position + TrigOffset (position + transform.position + offset);
-			else
-				return position + TrigOffset (position + offset);
-		}
+			return position + TrigOffset (position + axisOffset);
 	}
 
 	private Vector3 TrigOffset (Vector3 basePosition)
@@ -44,7 +48,28 @@ public class TrigModifier : MeshModifierBase
 
 	private Vector3 Trig3 (Vector3 t)
 	{
-		var offset = speed * modObject.Time;
-		return new Vector3 ((xAxis.amplitude == 0) ? 0f : xAxis.Solve (t.z, offset.x), (yAxis.amplitude == 0) ? 0f : yAxis.Solve (t.x, offset.y), (zAxis.amplitude == 0) ? 0f : zAxis.Solve (t.y, offset.z));
+		var animatedOffset = speed * modObject.Time;
+		var byValue = 0f;
+		switch (by)
+		{
+			case Axis.X:
+				byValue = t.x;
+				break;
+			case Axis.Y:
+				byValue = t.y;
+				break;
+			case Axis.Z:
+				byValue = t.z;
+				break;
+		}
+		switch (along)
+		{
+			case Axis.X:
+				return new Vector3 (trig.Solve (byValue, animatedOffset), 0f, 0f);
+			case Axis.Y:
+				return new Vector3 (0f, trig.Solve (byValue, animatedOffset), 0f);
+			default:
+				return new Vector3 (0f, 0f, trig.Solve (byValue, animatedOffset));
+		}
 	}
 }
--- Assets/MeshModifiers/Code/Scripts/ModifierObject.cs
+++ Assets/MeshModifiers/Code/Scripts/ModifierObject.cs
@@ -3,6 +3,7 @@ using UnityEngine.Events;
 using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace MeshModifiers
 {
