390be407da4b2d82c7cd7e34c795630572485838
keenanwoodall <keenanwoodall@gmail.com>
2017-11-09 11:37:42 -0600

    Added Spherize modifier

---
+++ Assets/MeshModifiers/Code/Scripts/MeshModifiers/SpherizeModifier.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace MeshModifiers
+{
+	[AddComponentMenu (MeshModifierConstants.ADD_COMP_BASE_NAME + "Spherize")]
+	public class SpherizeModifier : MeshModifierBase
+	{
+		[Range (0f, 1f)]
+		public float amount = 1f;
+		public float radius = 1f;
+
+		protected override Vector3 _ModifyOffset (VertexData vertexData)
+		{
+			return Vector3.Lerp (vertexData.position, vertexData.position.normalized * radius, amount);
+		}
+	}
+}
