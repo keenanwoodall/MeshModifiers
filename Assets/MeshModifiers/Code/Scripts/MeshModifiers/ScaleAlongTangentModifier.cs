b49351f330ceafee4f9cada7383ce17d6b567880
keenanwoodall <keenanwoodall@gmail.com>
2017-11-09 12:04:40 -0600

    Added Scale Along Tangent Modifier

---
+++ Assets/MeshModifiers/Code/Scripts/MeshModifiers/ScaleAlongTangentModifier.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace MeshModifiers
+{
+	[AddComponentMenu (MeshModifierConstants.ADD_COMP_BASE_NAME + "Tangent Scale")]
+	public class ScaleAlongTangentModifier : MeshModifierBase
+	{
+		public float amount;
+
+		protected override Vector3 _ModifyOffset (VertexData vertexData)
+		{
+			return vertexData.position + ((Vector3)vertexData.tangent * amount);
+		}
+	}
+}
