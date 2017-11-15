3def3dbff81d76a326e35c01646e8d6723bf9138
keenanwoodall <keenanwoodall@gmail.com>
2017-11-15 12:23:48 -0600

    Added Anti Modifier
    Anti modifier reverts the modified vertices back to their base position
    via a falloff curve, position, and radius.

---
+++ Assets/MeshModifiers/Code/Scripts/MeshModifiers/AntiModifier.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using MeshModifiers;
+
+namespace MeshModifiers
+{
+	// If you want to add this modifier to the component menu, uncomment the line below and type in your modifier name.
+	[AddComponentMenu (MeshModifierConstants.ADD_COMP_BASE_NAME + "Anti Modifier")]
+	public class AntiModifier : MeshModifierBase
+	{
+		public float strength = 0.5f;
+		public float radius = 0.5f;
+		public Vector3 position;
+		public AnimationCurve falloff = AnimationCurve.Linear (0f, 0f, 1f, 1f);
+
+		private void OnDrawGizmosSelected ()
+		{
+			Gizmos.DrawWireSphere (transform.position + (transform.rotation * position), radius);
+		}
+		protected override Vector3 _ModifyOffset (VertexData vertexData)
+		{
+			var distance = Vector3.Distance (vertexData.position, position);
+			if (distance < radius)
+			{
+				return Vector3.Lerp (vertexData.position, vertexData.basePosition, falloff.Evaluate ((1f - (distance / radius)) * strength));
+			}
+			return vertexData.position;
+		}
+	}
+}
